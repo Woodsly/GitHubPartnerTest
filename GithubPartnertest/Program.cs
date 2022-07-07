0832ef5e76454fffec725333055eeb1e1e43a893
VinhDang88 <vincedang720@gmail.com>
2022-07-07 14:39:29 -0400

    Vinh finished part 2

--- GithubPartnertest/Program.cs
+++ GithubPartnertest/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using GithubPartnertest;
+
+Employee x = new Employee("Justin", 100000);
+Employee y = new Employee("Josh", 200000);
+
+List<Employee> list = new List<Employee>();
+
+list.Add(x);
+list.Add(y);
+
+//foreach(Employee e in list)
+{
+    Console.WriteLine($"Name:{e.Name} Salary: {e.Salary}");
+}
+
+//This is your job. Get rid of things you should get rid of.
