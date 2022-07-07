c79f9d0e7d993753a2cd5f52314505b951108f56
Connor <Woodconnor13@gmail.com>
2022-07-07 14:28:44 -0400

    Created employee class

---
+++ GithubPartnertest/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GithubPartnertest
+{
+    internal class Employee
+    {
+        //properties
+        public string Name { get; set; }
+        public double Salary { get; set; }
+        public Employee(string _name, double _salary)
+        {
+            Name = _name;
+            Salary = _salary;
+        }
+    }
+}
